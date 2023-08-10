df88fa4446c907bfa909b288749a2e67ce7fbe80
Minsoek96 <FE.minseok96@gmail.com>
2023-08-11 02:22:42 +0900

    refactor : 토큰의 인증 유무 처리를 적용하고 필터된 적절한 페이지로 리다이렉트 처리 변경

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,17 +1,24 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 import { getTodosAPI } from '../api/todo';
 
 const useAuth = () => {
   const navigate = useNavigate();
+  const whiteList = ['/signin', '/signup'];
+  const location = useLocation();
   useEffect(() => {
-    getTodosAPI()
-      .then((res) => {
-        if (res.status === 200) {
-          navigate('/todo', { replace: true });
-        }
-      })
-      .catch((error) => navigate('/signin', { replace: true }));
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      getTodosAPI()
+        .then((res) => {
+          if (res.status === 200) {
+            navigate('/todo', { replace: true });
+          }
+        })
+        .catch((error) => navigate('/signin', { replace: true }));
+    } else if (!whiteList.includes(location.pathname)) {
+      navigate('/signin', { replace: true });
+    }
   }, []);
   return;
 };
